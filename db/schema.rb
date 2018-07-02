548c57d2ded0d3998eb221d7d697da10498f3d00
histfak@gmail.com <histfak@gmail.com>
2018-07-03 01:17:36 +0300

    part #3, foreign key added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_02_190859) do
+ActiveRecord::Schema.define(version: 2018_07_02_221331) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false

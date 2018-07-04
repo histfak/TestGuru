fe36a1dddd3ba214a9aea26e4e06b19b3bb971fb
histfak@gmail.com <histfak@gmail.com>
2018-07-05 00:38:16 +0300

    small fixes

--- db/migrate/20180702185614_add_tests_attrs_null_constraint.rb
+++ db/migrate/20180702185614_add_tests_attrs_null_constraint.rb
@@ -2,5 +2,6 @@ class AddTestsAttrsNullConstraint < ActiveRecord::Migration[5.2]
   def change
     change_column_null(:tests, :title, false)
     change_column_null(:tests, :level, false)
+    change_column_null(:tests, :category_id, false)
   end
 end
--- db/migrate/20180702190138_add_tests_category_id_null_constraint.rb
+++
@@ -1,5 +0,0 @@
-class AddTestsCategoryIdNullConstraint < ActiveRecord::Migration[5.2]
-  def change
-    change_column_null(:tests, :category_id, false)
-  end
-end

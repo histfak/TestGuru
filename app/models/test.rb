9d7ee0670259ac90640318d7b80de28ed5a91664
histfak@gmail.com <histfak@gmail.com>
2018-07-06 21:03:52 +0300

    part #4 first try

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+  def self.find_by_category(category)
+    where(category_id: Category.find_by(title: category).id).order(title: :desc).pluck(:title)
+    # or
+    # joins('JOIN categories ON (tests.category_id = categories.id)').where(categories: { title: category }).order(title: :desc).pluck(:title)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_02_221331) do
+ActiveRecord::Schema.define(version: 2018_07_06_171832) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 2018_07_02_221331) do
     t.string "title", null: false
     t.integer "level", default: 0, null: false
     t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_tests", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "test_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

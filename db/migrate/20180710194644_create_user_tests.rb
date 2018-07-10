082e5c36dbb3d663f0ef2942f8d7da3187eac66f
histfak@gmail.com <histfak@gmail.com>
2018-07-10 22:53:50 +0300

    part #4 final fixes

--- db/migrate/20180710194644_create_user_tests.rb
+++ db/migrate/20180710194644_create_user_tests.rb
@@ -1,8 +1,8 @@
 class CreateUserTests < ActiveRecord::Migration[5.2]
   def change
     create_table :user_tests do |t|
-      t.integer :user_id
-      t.integer :test_id
+      t.references :test, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_06_171832) do
+ActiveRecord::Schema.define(version: 2018_07_10_194644) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -42,10 +42,12 @@ ActiveRecord::Schema.define(version: 2018_07_06_171832) do
   end
 
   create_table "user_tests", force: :cascade do |t|
-    t.integer "user_id"
     t.integer "test_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["test_id"], name: "index_user_tests_on_test_id"
+    t.index ["user_id"], name: "index_user_tests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|

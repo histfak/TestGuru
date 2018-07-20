bb7743b5ec575c41fe760cc0eb0073383107a9aa
histfak@gmail.com <histfak@gmail.com>
2018-07-20 09:15:43 +0300

    part #7

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,48 @@
+class QuestionsController < ApplicationController
+  before_action :find_test
+  before_action :find_question_number, only: %i[destroy show]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_record_not_found
+
+  def index
+    render inline: '<% @test.questions.each { |q| %><p><%= q.body %></p> <% } %>'
+  end
+
+  def show
+    render inline: '<%= @test.questions[@question_number].body %>'
+  end
+
+  def new
+  end
+
+  def create
+    Question.create(prepare_question)
+    render plain: 'Done!'
+    byebug
+  end
+
+  def destroy
+    @test.questions[@question_number].destroy
+    render plain: 'Done!'
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question_number
+    @question_number = params[:id].to_i - 1
+  end
+
+  def prepare_question
+    @question_params = params.require(:question).permit(:body)
+    @question_params[:test_id] = params[:test_id]
+    @question_params
+  end
+
+  def rescue_with_record_not_found
+    render plain: 'Unable to find the record!'
+  end
+end

d4ae14138479c317458f822253408fec3a6a927e
histfak@gmail.com <histfak@gmail.com>
2018-07-24 22:04:24 +0300

    part #9 fix #1

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,2 +1,5 @@
 module TestPassagesHelper
+  def result_style(test_passage)
+    test_passage.success? ? 'success' : 'fail'
+  end
 end
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,13 +3,10 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
-  before_update :before_update_set_next_question
+  before_validation :before_validation_find_question
 
   def accept!(answer_ids)
-    if correct_answer?(answer_ids)
-      self.correct_questions += 1
-    end
+    self.correct_questions += 1 if correct_answer?(answer_ids)
     save!
   end
 
@@ -17,17 +14,22 @@ class TestPassage < ApplicationRecord
     current_question.nil?
   end
 
+  def success?
+    count_percents >= 85
+  end
+
   def current_position
-    test.questions.count - (test.questions.last.id - current_question.id)
+    test.questions.where('id < ?', current_question.id).count + 1
   end
 
   def count_percents
-    ((correct_questions.to_f / test.questions.count.to_f ) * 100).round
+    (correct_questions.to_f / test.questions.count * 100).round
   end
 
   private
 
   def correct_answer?(answer_ids)
+    return false if no_answers?(answer_ids)
     correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
@@ -35,15 +37,16 @@ class TestPassage < ApplicationRecord
     current_question.answers.correct
   end
 
+  def no_answers?(answer_ids)
+    answer_ids.nil?
+  end
+
   def next_question
+    return test.questions.first unless current_question
     test.questions.order(:id).where('id > ?', current_question.id).first
   end
 
-  def before_update_set_next_question
+  def before_validation_find_question
     self.current_question = next_question
-  end
-
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
   end
 end

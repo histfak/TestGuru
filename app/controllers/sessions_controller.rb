01d1edf7460e4f729e4a09546baad7b73f451dbd
histfak@gmail.com <histfak@gmail.com>
2018-07-28 05:43:23 +0300

    part #10

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  before_action :find_user, on: %i[create update]
+
+  def new
+  end
+
+  def create
+    if @user&.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to cookies[:path] || root_path
+    else
+      flash.now[:alert] = 'Are you a Guru? Verify your email and password please'
+      render :new
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_path
+  end
+
+  private
+
+  def find_user
+    @user = User.find_by(email: params[:email])
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,12 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passages
   has_many :own_tests, class_name: 'Test', foreign_key: :author_id
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, on: :create }
+  validates :name, presence: true, uniqueness: true
+
+  # validates :password, presence: true, if: Proc.new { |u| u.password_digest.blank? }
+  # validates :password, confirmation: true
+  has_secure_password
 
   def tests_taken_by_level(level)
     tests.by_level(level)

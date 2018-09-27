6b2825c24a293dd24eb6b4e7620bd0428c592e72
histfak@gmail.com <histfak@gmail.com>
2018-09-27 16:16:50 +0300

    part #15 hide issues

---
+++ app/assets/javascripts/utilities/password_matching.js
@@ -0,0 +1,19 @@
+document.addEventListener('turbolinks:load', function() {
+    var control = document.querySelector('#new_user')
+    if (control) {control.addEventListener('input', passwordMatching)}
+})
+
+function passwordMatching() {
+    var password = document.querySelector('#user_password')
+    var passwordConfirmation = document.querySelector('#user_password_confirmation')
+    var icon1 = document.querySelector('.octicon-check')
+    var icon2 = document.querySelector('.octicon-x')
+
+    if (passwordConfirmation.value === "") {
+        icon2.classList.remove('hide')
+    } else if (password.value === passwordConfirmation.value) {
+        icon1.classList.remove('hide')
+    } else {
+        icon2.classList.remove('hide')
+    }
+}

dcf4706a8d2a588253ed59602a036a67adc693ae
histfak@gmail.com <histfak@gmail.com>
2019-03-29 20:42:14 +0300

    part #19p2 timer before counting logic

--- app/models/test.rb
+++ app/models/test.rb
@@ -7,10 +7,15 @@ class Test < ApplicationRecord
 
   validates :title, presence: true, uniqueness: { scope: :level }
   validates :level, numericality: { greater_than_or_equal_to: 0 }
+  validates :timer, numericality: { greater_than_or_equal_to: 0 }
 
   scope :by_level, ->(level) { where(level: level) }
   scope :by_category, ->(category) { joins(:category).where(categories: { title: category }).order(title: :desc) }
   scope :easy, -> { by_level(0..1) }
   scope :medium, -> { by_level(2..4) }
-  scope :hard, -> { by_level(5..Float::INFINITY) }
+  scope :hard, -> { by_level(5..) }
+
+  def timer?
+    timer.positive?
+  end
 end

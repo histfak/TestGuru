13c612681de352a5184ac2b1ef1163e112a6d67a
histfak@gmail.com <histfak@gmail.com>
2019-03-29 23:16:25 +0300

    part #19p2 timer before checking

--- app/assets/javascripts/utilities/timer.js
+++ app/assets/javascripts/utilities/timer.js
@@ -2,6 +2,20 @@ document.addEventListener('turbolinks:load', function () {
     var timer = document.querySelector('.timer');
 
     if (timer) {
-        //
+        var timerTime = timer.dataset.timer;
+        var testPassageId = timer.dataset.testPassageId;
+
+        setInterval(function () {
+            if (timerTime > 0) {
+                timerTime--;
+            }
+
+            else {
+                alert('Время вышло!');
+                window.location.replace('/test_passages/' + testPassageId + '/result');
+            }
+
+            timer.innerHTML = parseInt(timerTime / 60) + ':' + (timerTime % 60 ? timerTime % 60 : '00');
+        }, 1000);
     }
 });
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -26,6 +26,10 @@ class TestPassage < ApplicationRecord
     (correct_questions.to_f / test.questions.count * 100).round
   end
 
+  def time_left
+    (expires_at - Time.current).to_i
+  end
+
   private
 
   def correct_answer?(answer_ids)
@@ -46,5 +50,9 @@ class TestPassage < ApplicationRecord
 
   def set_next_question
     self.current_question = next_question
+  end
+
+  def expires_at
+    created_at + test.timer.minutes
   end
 end
